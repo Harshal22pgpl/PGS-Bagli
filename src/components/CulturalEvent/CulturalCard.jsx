d05df76ddb548758e7bf98e8390fe64938f54bd8
Harshal22pgpl <harshalpgpl@gmail.com>
2024-07-18 12:43:06 +0530

    new modification

--- src/components/CulturalEvent/CulturalCard.jsx
+++ src/components/CulturalEvent/CulturalCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function CulturalCard({ img }) {
-  return <div className="w-full  border-2 border-gray-900 overflow-hidden rounded-2xl">
-<img className="" src={`/CULTURAL/1 (${img}).jpg`} alt={`${img}`} />
-  </div>;
+  return (
+    <div className="w-full  border-2 border-gray-900 overflow-hidden rounded-2xl">
+      <img className="" src={`/CULTURAL/1 (${img}).jpg`} alt={`${img}`} />
+    </div>
+  );
 }
